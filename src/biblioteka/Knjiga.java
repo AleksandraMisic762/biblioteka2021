9b54f85f94ae3b61a7afb33e2ff8b2eca922e6cf
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2021-03-01 14:42:02 +0100

    uvedena klasa Autor i izmenjena klasa Knjiga

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,9 +1,11 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 public class Knjiga {
 	
 	private String naslov;
-	private String autori;
+	private LinkedList<Autor> autori;
 	private String isbn;
 	private String izdavac;
 	private int izdanje;
@@ -17,11 +19,11 @@ public class Knjiga {
 		this.naslov = naslov;
 	}
 	
-	public String getAutori() {
+	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
 	
-	public void setAutori(String autori) {
+	public void setAutori(LinkedList<Autor> autori) {
 		this.autori = autori;
 	}
 	
